991e2797665c22bd487c3e8fcbd7032386e27166
Santhoshraju-ch <santhosh.ch192@gmail.com>
2026-01-08 14:50:54 +0530

    add prodeservice class

---
+++ codeassigements/src/main/java/com/codeassigements/services/ProbeService.java
@@ -0,0 +1,10 @@
+package com.codeassigements.services;
+
+import org.springframework.stereotype.Service;
+
+import com.codeassigements.model.Grid;
+import com.codeassigements.model.Probe;
+
+@Service
+public class ProbeService {
+}
