d2789e7755f836be1d1127b6d2dbca034d801dd3
Santhoshraju-ch <santhosh.ch192@gmail.com>
2026-01-08 14:53:59 +0530

    add fun directionleft,right

--- codeassigements/src/main/java/com/codeassigements/enu/Directions.java
+++ codeassigements/src/main/java/com/codeassigements/enu/Directions.java
@@ -2,6 +2,13 @@ package com.codeassigements.enu;
 
 public enum Directions {
 
-	NORTH, WEST, SOUTH, EAST,
+	NORTH, WEST, SOUTH, EAST;
 
+	public Directions left() {
+		return values()[(ordinal() + 3) % 4];
+	}
+
+	public Directions right() {
+		return values()[(ordinal() + 1) % 4];
+	}
 }
