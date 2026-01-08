09b7ccb6ab9b3ca971a4d502b921b7616e30c3b1
Santhoshraju-ch <santhosh.ch192@gmail.com>
2026-01-08 14:16:19 +0530

    Initial Spring Boot project setup

---
+++ codeassigements/src/main/java/com/codeassigements/CodeassigementsApplication.java
@@ -0,0 +1,13 @@
+package com.codeassigements;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeassigementsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeassigementsApplication.class, args);
+	}
+
+}
---
+++ codeassigements/src/test/java/com/codeassigements/CodeassigementsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeassigements;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeassigementsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
