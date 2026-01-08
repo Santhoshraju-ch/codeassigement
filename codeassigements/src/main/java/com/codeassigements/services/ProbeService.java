f18e3948786afcfdffa5c9c726b48a47c7d777c2
Santhoshraju-ch <santhosh.ch192@gmail.com>
2026-01-08 14:59:56 +0530

    add prodeservice logics

--- codeassigements/src/main/java/com/codeassigements/services/ProbeService.java
+++ codeassigements/src/main/java/com/codeassigements/services/ProbeService.java
@@ -7,4 +7,34 @@ import com.codeassigements.model.Probe;
 
 @Service
 public class ProbeService {
+	public Probe execute(Grid grid, Probe probe, String commands) {
+
+		for (char command : commands.toCharArray()) {
+			switch (command) {
+			case 'L' -> probe.setDirection(probe.getDirection().left());
+			case 'R' -> probe.setDirection(probe.getDirection().right());
+			case 'F' -> move(grid, probe, 1);
+			case 'B' -> move(grid, probe, -1);
+			}
+		}
+		return probe;
+	}
+
+	private void move(Grid grid, Probe probe, int step) {
+		int newX = probe.getX();
+		int newY = probe.getY();
+
+		switch (probe.getDirection()) {
+		case NORTH -> newY += step;
+		case SOUTH -> newY -= step;
+		case EAST -> newX += step;
+		case WEST -> newX -= step;
+		}
+
+		if (grid.isValid(newX, newY)) {
+			probe.setX(newX);
+			probe.setY(newY);
+			probe.visit();
+		}
+	}
 }
