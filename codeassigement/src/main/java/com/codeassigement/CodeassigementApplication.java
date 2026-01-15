5b951daebfcc9858dd2612e2aac8a95c06afc7c9
Santhoshraju-ch <santhosh.ch192@gmail.com>
2026-01-15 11:03:56 +0530

    init spring boot created project

---
+++ codeassigement/src/main/java/com/codeassigement/CodeassigementApplication.java
@@ -0,0 +1,13 @@
+package com.codeassigement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodeassigementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeassigementApplication.class, args);
+	}
+
+}
---
+++ codeassigement/src/test/java/com/codeassigement/CodeassigementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeassigement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeassigementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
