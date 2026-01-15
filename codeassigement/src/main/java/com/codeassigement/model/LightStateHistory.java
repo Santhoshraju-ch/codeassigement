0d2114eae0c6c6155472efad9410c2f6b6971066
Santhoshraju-ch <santhosh.ch192@gmail.com>
2026-01-15 12:59:52 +0530

    created LightStateHistory class

---
+++ codeassigement/src/main/java/com/codeassigement/model/LightStateHistory.java
@@ -0,0 +1,27 @@
+package com.codeassigement.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import com.codeassigement.enu.Direction;
+import com.codeassigement.enu.LightColor;
+
+public class LightStateHistory {
+
+    private final LocalDateTime timestamp;
+    private final Map<Direction, LightColor> state;
+
+    public LightStateHistory(LocalDateTime timestamp,
+                             Map<Direction, LightColor> state) {
+        this.timestamp = timestamp;
+        this.state = state;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public Map<Direction, LightColor> getState() {
+        return state;
+    }
+}
